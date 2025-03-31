f2ab502c926a21921e714fd3cebb547a2abc4aac
sage-natsu <62790701+sage-natsu@users.noreply.github.com>
2025-04-01 07:43:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -607,7 +607,7 @@ def main():
                             st.session_state.cleaned_data = df_cleaned  # ✅ Store in session state
                             st.session_state.data_uploaded = True
 
-xs
+
            		    # ✅ Debug: Print Column Names and Sample Rows
                             st.write("🔍 Debugging: CSV Columns Detected:")
                             st.write(df_cleaned.columns.tolist())
