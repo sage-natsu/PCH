92026d6b092ecc9cc096c2d3fd1bc8514e235e43
sage-natsu <62790701+sage-natsu@users.noreply.github.com>
2025-05-15 06:39:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -633,7 +633,7 @@ def main():
                 .drop_duplicates(subset=["Title", "Body"], keep="last")
                 .reset_index(drop=True)
                 )                
-		st.write(f"Total fetched records: {len(all_posts_df)}")   
+                st.write(f"Total fetched records: {len(all_posts_df)}")   
                 st.write(f"Time taken to fetch records: {elapsed_time:.2f} seconds")  # Display the elapsed time    
                 st.subheader("All Posts")
                 st.dataframe(all_posts_df)
