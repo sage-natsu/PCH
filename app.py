932f0184fc7f7052b9a2610c74cbf1cbd8563ad7
sage-natsu <62790701+sage-natsu@users.noreply.github.com>
2025-05-20 04:22:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ async def fetch_praw_data(queries, start_date_utc, end_date_utc, limit=50, subre
                 for dis in disability_terms:
                     if dis != "ADD" and re.search(rf"\b{re.escape(dis.lower())}\b", combined_lower):
                         detected_dis.append(dis)
-		sub = submission.subreddit    
+                sub = submission.subreddit    
                 # ✅ Prepare the post data with mandatory fields
                 post_data = {
                     "Post ID": submission.id,
