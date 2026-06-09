82b1c0d1fc314a23bad9127aa318a3d77066c3a9
sage-natsu <62790701+sage-natsu@users.noreply.github.com>
2026-06-09 12:42:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ async def fetch_praw_data(queries, start_date_utc, end_date_utc, limit=500, subr
              try:
                 async for submission in subreddit_instance.search(
                     query,
-                    limit=1    1000,              # ← no max, pull as many as Reddit will give you
+                    limit= 1000,              # ← no max, pull as many as Reddit will give you
                     sort="relevance", 
                     syntax="lucene"
                 ):
